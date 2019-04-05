95aa1480f33c8095b2714e21fc4a83059a4ec7d3
pci2676 <pci2676@gmail.com>
2019-04-06 00:16:50 +0900

    프로젝트 생성
    스웨거추가

---
+++ src/main/java/org/dailystudio/onepiece/OnepieceApplication.java
@@ -0,0 +1,13 @@
+package org.dailystudio.onepiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepieceApplication.class, args);
+	}
+
+}
